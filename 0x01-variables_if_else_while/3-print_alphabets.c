2cbd048cc6793d70f8d0a83279cd6fb5b9e96d45
ericpepo <pepoeric10@gmail.com>
2023-02-20 22:59:08 +0300

    task3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <studio.h>
+#include<ctype.h>
+#include<stdlib.h>
+
+/**
+  *main -entry point
+  *
+  *Return: Always 0
+  */
+int main(void)
+{
+	int x;
+
+	for (x = 'A'; x <= 'Z'; x++)
+	{
+		putchar(tolower(x));
+
+	}
+	for (x = 'A'; x <= 'Z'; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
